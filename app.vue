66623281ba0a958aa4901ad7c59001ae6dd80e6f
Paul Faass <paulfaass@gmail.com>
2024-01-11 20:00:14 +0100

    feat: create a basic layout with two inputs and an action button

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container max-w-5xl py-32 px-8">
+    <div class="flex flex-col md:flex-row gap-8 mb-8">
+      <UTextarea class="flex-1" v-model="input1" />
+      <UTextarea class="flex-1" v-model="input2" />
+    </div>
+    <UButton @click="calculateSimilarity">Calculate similarity</UButton>
+    <p class="mt-8 text-3xl">
+      Output: <span class="font-bold">{{ similarity }}</span>
+    </p>
   </div>
 </template>
+
+<script setup lang="ts">
+
+const input1 = ref('')
+const input2 = ref('')
+
+const similarity = ref(0)
+
+function calculateSimilarity() {
+  similarity.value++
+  console.log(similarity)
+}
+
+</script>
